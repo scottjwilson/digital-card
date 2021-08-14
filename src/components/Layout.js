f5ec7c6ab3dd15f2536dcf41a02ac96ec6e350fb
scott <slimzel@protonmail.com>
2021-08-14 14:38:05 -0700

    random test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { GlobalStyles } from "twin.macro";
-
+import "./index.css";
 const Layout = ({ children, ...rest }) => (
-  <div {...rest}>
+  <div {...rest} className="h-screen h-auto">
     <GlobalStyles />
     {children}
   </div>
